3f2c39a4b8c870c78c26983c04e1f72e82a57085
BrunaGCD <145371028+BrunaGCD@users.noreply.github.com>
2023-09-22 17:45:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-import math 
+import math
+import os
+from time import sleep
 #Lembrete: Se possível, colocar um indicador no menu quando uma função já está disponível ou não (Talvez um "X" do lado para quando não estiver disponível ou algo do tipo). Acho que ficaria bonitinho no resultado final.
+
 
 #/\ COMENTÁRIOS DE LEMBRETE TEMPORÁRIOS ACIMA /\
 
+#Variáveis globais, temporariamente?
+xSup, ySup = 0, 0 #Coordenadas do canto superior esquerdo da fazenda
+xInf, yInf = 0, 0 #Coordenadas do canto inferior direito da fazenda
+xSed, ySed = 0, 0 #Coordenadas da sede UPMCC
 
-def coordenadas(): #FALTA MEU PROCESSO DE OTIMIZAR (INCOMPLETO)
-  print("Informe as coordenadas dos dois cantos opostos da área da sua fazenda.")
-  print("Canto superior esquerdo:", end=" ")
-  print("Canto inferior direito:", end=" ")
-  
-
-def converter(medida, angulo):
-  # aplicando formula para converter coordenadas cartesianas em polares
-  r = math.sqrt((medida ** 2) + (angulo ** 2))
-  print(r)
-  
-# Construindo um menu 
-def main():
+def mostrarMenu():
   print("\n*****************") 
   print("Chuva de Meteoros")
   print("*****************")
@@ -24,33 +19,69 @@ def main():
   print('''
   Sistema para analíse de Meteoros
   Menu: 
-  [1] Definir perímetro da propriedade e da edificação de interesse
-  [2] Unificar sistemas de coordenadas de referência
-  [3] Processar registros de chuvas de meteoros
-  [4] Apresentar estatísticas
-  [5] Sair
-  ''')
+  [1] Definir a localização da fazenda e da sede
+  [2] Processar registros de chuvas de meteoros
+  [3] Apresentar estatísticas
+  [4] Sair
+  ''') #Função para printar menu
+
+def converterParaCoord(): #FALTA MEU PROCESSO DE OTIMIZAR (INCOMPLETO)
+  pass
+  
+  
+''' SALVANDO CÓDIGO
+def converter(medida, angulo):
+  # aplicando formula para converter coordenadas cartesianas em polares
+  r = math.sqrt((medida ** 2) + (angulo ** 2))
+  print(r)
+'''
+
+# Construindo um menu 
+def main():
+  mostrarMenu()
   
   opc = ''
-  while opc != '5': #Enquanto o usuário já não tiver saído,
+  while opc != '4': #Enquanto o usuário já não tiver saído,
     print("Opção:", end=" ") #Pergunto a opção
     opc = input()
     
     if opc == '1':
-      #coordenadas()
-      print("Medida:", end=" ")
-      m = float(input())
-      print("Ângulo:", end=" ")
-      a = float(input())
-      converter(m, a)
-    elif opc == '2': 
+      sleep(0.2)
+      os.system('clear')
+      print("Informe as coordenadas dos dois cantos opostos da área da sua fazenda.")
+      while True: #Validando entrada do usuário
+        try:
+          print("\nDigite no seguinte formato: x, y")
+          print("Canto superior esquerdo:", end=" ")
+          xSup, ySup = map(float, input().split(", "))
+          print("Canto inferior direito:", end=" ")
+          xInf, yInf = map(float, input().split(", "))
+          break
+        except:
+          print("Formato inválido, tente novamente.")
+
+      print("\nInforme as coordenadas da localização da sede UPMCC.")
+      while True:
+        try:
+          print("\nDigite no seguinte formato: x, y")
+          print("Localização:", end=" ")
+          xSed, ySed = map(float, input().split(", "))
+          break
+        except:
+          print("Formato inválido, tente novamente.")
+
+      print("\nVoltando ao menu...")
+      sleep(1.5)
+      os.system('clear')
+      mostrarMenu()
+      
+    elif opc == '2':
+      print()
       pass
-    elif opc == '3': 
+    elif opc == '3':
       pass
-    elif opc == '4':
-      pass
-    elif opc == '5': 
-      #A partir do momento que opc agora é igual a 5,
+    elif opc == '4': 
+      #A partir do momento que opc agora é igual a 4,
       #O Programa irá se encerrar automaticamente,
       #"break" não é necessário.
       print("Saindo...")
@@ -59,3 +90,7 @@ def main():
 
 
 main()
+
+
+  
+  
